bed7c3fbb9ad1e658733b94292395d87b2e163f7
Lucas Lira <lucasliradev@gmail.com>
2024-09-25 13:28:46 -0300

    Redirecionando para repositories com Link - react-router-dom

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, {useState} from "react";
 import { MdSearch } from "react-icons/md"
 
 import {Button, Container, Form, Input, LogoGit, Title} from "./styles";
 import githubLogo from "../../assets/images/github-logo.svg"
 
 export default function Main() {
+  const [ login, setLogin ] = useState('');
+
   return (
     <Container>
       <LogoGit
@@ -15,8 +17,12 @@ export default function Main() {
         API GitHub
       </Title>
       <Form>
-        <Input placeholder="usuário"/>
-        <Button>
+        <Input
+          placeholder="usuário"
+          value={login}
+          onChange={e => setLogin(e.target.value)}
+        />
+        <Button to={`/${login}/repositories`}>
           <MdSearch className="icon" size={42}/>
         </Button>
       </Form>
