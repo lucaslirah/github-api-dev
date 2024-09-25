6929c361a2e61320cde50c4ffa31111decd2c3cb
Lucas Lira <lucasliradev@gmail.com>
2024-09-25 12:19:32 -0300

    Tela de busca - ajustando estilos globais e da página Main

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
+import { MdSearch } from "react-icons/md"
 
-import { Container, LogoGit} from "./styles";
+import {Button, Container, Form, Input, LogoGit, Title} from "./styles";
 import githubLogo from "../../assets/images/github-logo.svg"
 
 export default function Main() {
@@ -8,8 +9,17 @@ export default function Main() {
     <Container>
       <LogoGit
         src={githubLogo}
-        alt="API Github"
+        alt="API GitHub"
       />
+      <Title>
+        API GitHub
+      </Title>
+      <Form>
+        <Input placeholder="usuário"/>
+        <Button>
+          <MdSearch className="icon" size={42}/>
+        </Button>
+      </Form>
     </Container>
   );
 }
