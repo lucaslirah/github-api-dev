f9cb8eb7f5710e7878ee416a57f2490fc56745cd
Lucas Lira <lucasliradev@gmail.com>
2024-09-27 10:11:20 -0300

    Filtros - criando estrutura

---
+++ src/pages/RepositoriesPage/Filter/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+function Filter() {
+  const langs = [
+    {name: 'JavaScript', count: 5, color: '#f1c40f'},
+    {name: 'Shell', count: 2, color: '#95a5a6'},
+    {name: 'PHP', count: 1, color: '#3498db'},
+  ]
+
+  return (
+    <Container>
+      {}
+    </Container>
+  )
+}
+
+export default Filter;
