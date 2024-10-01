7425e2bd081913685d1e03240e2b908a59347f8c
Lucas Lira <lucasliradev@gmail.com>
2024-10-01 10:48:12 -0300

    Dados para o Profile

--- src/pages/RepositoriesPage/Profile/index.jsx
+++ src/pages/RepositoriesPage/Profile/index.jsx
@@ -1,36 +1,59 @@
 import React from 'react';
+import PropTypes from "prop-types";
 import { MdGroup, MdWork, MdLocationCity, MdLink } from 'react-icons/md';
 
 import {Avatar, Container, Data, Header, Inner, Login, Name} from './styles';
 
-function Profile() {
+function Profile({user}) {
   return (
     <Container>
       <Header>
-        <Avatar src="https://avatars.githubusercontent.com/u/99286728?v=4"/>
-        <Login>lucaslirah</Login>
-        <Name>Lucas Lira</Name>
+        <Avatar src={user.avatar_url}/>
+        <Login>{user.login}</Login>
+        <Name>{user.name}</Name>
       </Header>
       <Inner>
         <Data>
           <MdGroup size={20}/>
-          &nbsp;30&nbsp;<i>seguidores</i>&nbsp;&middot;&nbsp;10&nbsp;<i>seguindo</i>
+          &nbsp;{user.followers}&nbsp;<i>seguidores</i>&nbsp;&middot;&nbsp;{user.following}&nbsp;<i>seguindo</i>
         </Data>
-        <Data>
-          <MdWork size={20}/>
-          &nbsp;<i>e-dinheiro</i>
-        </Data>
-        <Data>
-          <MdLocationCity size={20}/>
-          &nbsp;<i>Fortaleza, CE</i>
-        </Data>
-        <Data>
-          <MdLink size={20}/>
-          <a href="https://github.com/lucaslirah">/lucaslirah</a>
-        </Data>
+        {
+          user.company &&
+          <Data>
+            <MdWork size={20}/>
+            &nbsp;<i>{user.company}</i>
+          </Data>
+        }
+        {
+          user.location &&
+          <Data>
+            <MdLocationCity size={20}/>
+            &nbsp;<i>{user.location}</i>
+          </Data>
+        }
+        {
+          user.blog &&
+          <Data>
+            <MdLink size={20}/>
+            <a href={`\\${user.blog}`} target="_blank" rel="noreferrer">{user.blog}</a>
+          </Data>
+        }
       </Inner>
     </Container>
   )
+}
+
+Profile.propTypes = {
+  user: PropTypes.shape({
+    login: PropTypes.string.isRequired,
+    avatar_url: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    location: PropTypes.string,
+    blog: PropTypes.string,
+    followers: PropTypes.number.isRequired,
+    following: PropTypes.number.isRequired,
+    company: PropTypes.string,
+  }).isRequired,
 }
 
 export default Profile;
--- src/pages/RepositoriesPage/index.jsx
+++ src/pages/RepositoriesPage/index.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
+
 import {Container, Main, Sidebar} from "./styles";
+
 import Profile from "./Profile";
 import Filter from "./Filter";
 import Repositories from "./Repositories";
+
+const user = {
+  login: "lucaslirah",
+  avatar_url: "https://avatars.githubusercontent.com/u/99286728?v=4",
+  name: "Lucas Lira",
+  company: "@edinheirobrasil",
+  blog: "lucasliradev@gmail.com",
+  location: "Fortaleza, Brazil",
+  followers: 5,
+  following: 6,
+}
 
 function RepositoriesPage() {
   return (
     <Container>
       <Sidebar>
-        <Profile />
+        <Profile user={user}/>
         <Filter/>
       </Sidebar>
       <Main>
